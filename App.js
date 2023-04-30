39bc789939e23c1d25ab827a7659e3eded46f5e5
Ri-Hong <riri.hong@gmail.com>
2023-04-30 02:11:26 -0400

    Finished UI

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   Pressable,
   Modal,
   Text,
+  Image,
   ActivityIndicator,
 } from "react-native";
 
@@ -15,6 +16,10 @@ import {
   startPrediction,
 } from "./helpers/tensor-helper";
 import { cropPicture } from "./helpers/image-helper";
+
+import Icon from 'react-native-vector-icons/FontAwesome'
+import IconMCI from 'react-native-vector-icons/MaterialCommunityIcons'
+import takephotobutton from "./images/takephotobutton.png";
 
 import { Camera } from "expo-camera";
 
@@ -51,8 +56,26 @@ export default function App() {
       <Modal visible={isProcessing} transparent={true} animationType="slide">
         <View style={styles.modal}>
           <View style={styles.modalContent}>
-            <Text>Your current shape is {presentedShape}</Text>
-            {presentedShape === "" && <ActivityIndicator size="large" />}
+            <Text style={styles.thisgoesin}>This item is...</Text>
+            {/* <Text style={styles.category}>{presentedShape}</Text> */}
+            {presentedShape === "Recyclable" ? (
+              <View style={styles.categoryContainer}>
+                <Icon name="recycle" size={50} color="#065FB1" />
+                <Text style={[{ color: "#065FB1" }, styles.category]}>Recyclable</Text>
+              </View>
+            ) : presentedShape === "Garbage" ? (
+              <View style={styles.categoryContainer}>
+                <Icon name="trash" size={50} color="#898987" />
+                <Text style={[{ color: "#898987", marginTop: 5 }, styles.category]}>Garbage</Text>
+              </View>
+            ) : (
+              <View style={styles.categoryContainer}>
+                <IconMCI name="food-apple" size={50} color="#61D94C" />
+                <Text style={[{ color: "#61D94C", marginTop: 5 }, styles.category]}>Compostable</Text>
+              </View>
+            )}
+
+            {/* {presentedShape === "" && <ActivityIndicator size="large" />} */}
             <Pressable
               style={styles.dismissButton}
               onPress={() => {
@@ -60,7 +83,7 @@ export default function App() {
                 setIsProcessing(false);
               }}
             >
-              <Text>Dismiss</Text>
+              <Text style={styles.dismissText}>Dismiss</Text>
             </Pressable>
           </View>
         </View>
@@ -73,10 +96,23 @@ export default function App() {
         autoFocus={true}
         whiteBalance={Camera.Constants.WhiteBalance.auto}
       ></Camera>
+
+      {/* <TouchableOpacity 
+        title="Predict"
+        onPress={()=>{handleImageCapture()}}
+        type="outline"
+        style={styles.captureButton}
+      >
+        <Image source={takephotobutton}  style={styles.img}/>
+      </TouchableOpacity> */}
+
       <Pressable
         onPress={() => handleImageCapture()}
         style={styles.captureButton}
-      ></Pressable>
+      >
+        <Image source={takephotobutton}  style={styles.img}/>
+      </Pressable>
+
     </View>
   );
 }
@@ -95,11 +131,11 @@ const styles = StyleSheet.create({
     position: "absolute",
     left: Dimensions.get("screen").width / 2 - 50,
     bottom: 40,
-    width: 100,
+    width: 80,
+    height: 80,
     zIndex: 100,
-    height: 100,
-    backgroundColor: "white",
-    borderRadius: 50,
+    // backgroundColor: "white",
+    borderRadius: 100,
   },
   modal: {
     flex: 1,
@@ -110,20 +146,47 @@ const styles = StyleSheet.create({
   },
   modalContent: {
     alignItems: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
     width: 300,
     height: 300,
     borderRadius: 24,
-    backgroundColor: "gray",
+    backgroundColor: "black",
+    opacity: 0.8
   },
   dismissButton: {
     width: 150,
     height: 50,
-    marginTop: 60,
+    marginTop: 45,
     borderRadius: 24,
     color: "white",
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "red",
+  },
+  dismissText: {
+    fontSize: 20
+  },
+  thisgoesin: {
+    fontSize: 25,
+    padding: 20,
+    marginTop: 30,
+    fontWeight: 'bold',
+    fontFamily: "Roboto",
+    color: '#FFFFFF'
+  },
+  category: {
+    fontSize: 30,
+    marginLeft: 10,
+    fontWeight: 'bold'
+  },
+  categoryContainer: {
+    paddingTop: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    // alignContent: 'center'
+    height: 60,
+    // borderColor: 'red',
+    // borderWidth: 2
   },
 });
